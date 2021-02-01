6fcc5b432a1bc490956a9c312e5f798d318d6a00
Fernando de Paz <f.depaz@hotmail.com>
2021-02-01 14:24:08 -0800

    working online

---
+++ Assets/Lobby_RoomPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Mirror;
+
+public class Lobby_RoomPlayer : NetworkRoomPlayer
+{
+    static readonly ILogger logger = LogFactory.GetLogger(typeof(Lobby_RoomPlayer));
+    
+
+    public override void OnStartClient()
+    {
+        if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "OnStartClient {0}", SceneManager.GetActiveScene().path);
+
+        base.OnStartClient();
+    }
+
+    public override void OnClientEnterRoom()
+    {
+        if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "OnClientEnterRoom {0}", SceneManager.GetActiveScene().path);
+        Debug.Log("hi");
+        Transform playerList = GameObject.Find("Players").transform;
+        playerList.GetChild(index).gameObject.SetActive(true);
+        playerList.GetChild(index).GetComponentInChildren<Button>().onClick.AddListener(readyUp);
+    }
+
+    public override void OnClientExitRoom()
+    {
+        if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "OnClientExitRoom {0}", SceneManager.GetActiveScene().path);
+    }
+
+    public override void ReadyStateChanged(bool _, bool newReadyState)
+    {
+        if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "ReadyStateChanged {0}", newReadyState);
+    }
+
+    public void readyUp() {
+        CmdChangeReadyState(true);
+    }
+}
---
+++ Assets/roomManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Mirror;
+public class roomManager : NetworkRoomManager
+{
+    /// <summary>
+    /// Called just after GamePlayer object is instantiated and just before it replaces RoomPlayer object.
+    /// This is the ideal point to pass any data like player name, credentials, tokens, colors, etc.
+    /// into the GamePlayer object as it is about to enter the Online scene.
+    /// </summary>
+    /// <param name="roomPlayer"></param>
+    /// <param name="gamePlayer"></param>
+    /// <returns>true unless some code in here decides it needs to abort the replacement</returns>
+    /// 
+    public GameObject StartButton;
+    public override bool OnRoomServerSceneLoadedForPlayer(NetworkConnection conn, GameObject roomPlayer, GameObject gamePlayer)
+    {
+        //PlayerScore playerScore = gamePlayer.GetComponent<PlayerScore>();
+        //playerScore.index = roomPlayer.GetComponent<NetworkRoomPlayer>().index;
+        return true;
+    }
+
+    public override void OnRoomStopClient()
+    {
+        // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
+        // going to the offline scene to avoid collision with the one that lives there.
+        if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
+            SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+
+        base.OnRoomStopClient();
+    }
+
+    public override void OnRoomStopServer()
+    {
+        // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
+        // going to the offline scene to avoid collision with the one that lives there.
+        if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
+            SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+
+        base.OnRoomStopServer();
+    }
+
+    /*
+        This code below is to demonstrate how to do a Start button that only appears for the Host player
+        showStartButton is a local bool that's needed because OnRoomServerPlayersReady is only fired when
+        all players are ready, but if a player cancels their ready state there's no callback to set it back to false
+        Therefore, allPlayersReady is used in combination with showStartButton to show/hide the Start button correctly.
+        Setting showStartButton false when the button is pressed hides it in the game scene since NetworkRoomManager
+        is set as DontDestroyOnLoad = true.
+    */
+
+    bool showStartButton;
+
+    public override void OnRoomServerPlayersReady()
+    {
+        // calling the base method calls ServerChangeScene as soon as all players are in Ready state.
+        if (isHeadless)
+            base.OnRoomServerPlayersReady();
+        else
+        {
+            showStartButton = true;
+            StartButton.SetActive(true);
+        }
+            
+
+    }
+
+    public override void OnGUI()
+    {
+        base.OnGUI();
+
+        if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+        {
+            // set to false to hide it in the game scene
+            showStartButton = false;
+            StartButton.SetActive(false);
+            ServerChangeScene(GameplayScene);
+        }
+    }
+
+    public void HostGame() {
+        StartHost();
+    
+    }
+    
+    public void StartGame()
+    {
+        if (allPlayersReady && showStartButton)
+        {
+            ServerChangeScene(GameplayScene);
+        }
+    }
+}
+
